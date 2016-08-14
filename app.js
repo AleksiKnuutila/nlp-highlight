427ab183bb2148dd19d77b84893ee760057cf5cc
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-08-14 19:45:18 +0200

    trying with jquery

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import User from "./src/model/user";
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.jQuery = require('jquery');
 import uikit from "uikit";
 import findAndReplaceDOMText from "findAndReplaceDOMText";
 
