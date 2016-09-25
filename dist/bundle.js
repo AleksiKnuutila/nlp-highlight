1fdf3dfa4519e3298270187943dd4b861c3c1a55
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2016-09-25 12:12:56 +0100

    bundle apth

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // This file can be generated with scripts/getjson.js
-const NLP_DATA_FILE = 'dist/clarkson.json';
+const NLP_DATA_FILE = '/nlp-highlight/dist/clarkson.json';
 
 window.jQuery = require('jquery');
 var Mustache = require('mustache');
